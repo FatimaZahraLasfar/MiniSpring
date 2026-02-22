0b124632fd2edd60a23e57961c018e132a55dd63
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-02-22 23:29:54 +0000

    L'ajout de l'interafce DAO et l'implementer

---
+++ src/main/java/ma/enset/dao/DaoImpl.java
@@ -0,0 +1,10 @@
+package ma.enset.dao;
+
+import ma.enset.framework.annotations.Component;
+
+@Component
+public class DaoImpl implements IDao {
+    public double getData() {
+        return 20;
+    }
+}
